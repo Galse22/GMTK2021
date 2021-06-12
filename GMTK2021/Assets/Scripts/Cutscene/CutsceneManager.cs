8cd0001a155f36160ba08b0aeb9fd241b12ac0b6
Galse22 <al.g.se622@gmail.com>
2021-06-12 18:41:59 -0300

    Start Cutscenes added

---
+++ GMTK2021/Assets/Scripts/Cutscene/CutsceneManager.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class CutsceneManager : MonoBehaviour
+{
+    public Sprite[] imageArray;
+    public Image baseImage;
+    public int sceneNumber;
+    int amountsButton;
+
+    public void ChangeImage()
+    {
+        amountsButton++;
+        if(amountsButton < imageArray.Length)
+        {
+            baseImage.overrideSprite = imageArray[amountsButton];
+        }
+        else
+        {
+            ActualLoadScene();
+        }
+    }
+
+    void ActualLoadScene()
+    {
+        SceneManager.LoadScene(sceneNumber);
+    }
+}
