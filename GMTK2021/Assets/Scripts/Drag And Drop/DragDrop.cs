75a83384a8f8e541f28f89adb1b835398ceabe23
Galse22 <al.g.se622@gmail.com>
2021-06-12 12:36:07 -0300

    Drag and DropV1

---
+++ GMTK2021/Assets/Scripts/Drag And Drop/DragDrop.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class DragDrop : MonoBehaviour, IPointerDownHandler, IBeginDragHandler, IEndDragHandler, IDragHandler
+{
+    public Canvas canvas;
+    private RectTransform rectTransform;
+    private CanvasGroup canvasGroup;
+    private void Awake() {
+        rectTransform = GetComponent<RectTransform>();
+        canvasGroup = GetComponent<CanvasGroup>();
+    }
+
+    // called when player begins dragging
+    public void OnBeginDrag(PointerEventData eventData)
+    {
+        Debug.Log("BeginDrag");
+        canvasGroup.blocksRaycasts = false;
+    }
+
+    // called when dragging
+    public void OnDrag(PointerEventData eventData)
+    {
+        Debug.Log("Dragging");
+        rectTransform.anchoredPosition += eventData.delta / canvas.scaleFactor;
+    }
+
+    // called when dragging is stopped
+    public void OnEndDrag(PointerEventData eventData)
+    {
+        Debug.Log("EndDrag");
+        canvasGroup.blocksRaycasts = true;
+    }
+
+    // called when mouse is pressed whilst on top of object
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        Debug.Log("PointerDown");
+    }
+}
---
+++ GMTK2021/Assets/Scripts/Drag And Drop/ItemSlot.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class ItemSlot : MonoBehaviour, IDropHandler
+{
+    public string desiredName;
+    private Image thisImage;
+    public void OnDrop(PointerEventData eventData)
+    {
+        Debug.Log("Drop");
+        if( eventData.pointerDrag.name == desiredName )
+        {
+            thisImage = GetComponent<Image>();
+            thisImage.sprite = eventData.pointerDrag.GetComponent<Image>().sprite;
+            thisImage.SetNativeSize();
+            eventData.pointerDrag.SetActive(false);
+            //thisRect = GetComponent<RectTransform>();
+            //otherRect = eventData.pointerDrag.GetComponent<RectTransform>();
+            //otherRect.anchoredPosition.x = thisRect.anchoredPosition.x + offset.x;
+            //otherRect.anchoredPosition.y = thisRect.anchoredPosition.y + offset.y;
+        }
+    }
+}
